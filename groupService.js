a5d03afcdd9599128d227f1c4db27238451ce747
Tommy Brown <46692709+tbrowndev@users.noreply.github.com>
2020-08-12 00:36:18 -0500

    added logic for getting group members, milestones and deleting groups alo minor fixes to coding errors

--- groupService.js
+++ groupService.js
@@ -69,7 +69,10 @@ app.get('/groups/search/:search', function (req, res) {
     try {
         let query = "CALL search_for_groups(\"" + search_term + "\");";
         connection.query(query, function (err, result) {
-            if (err) { throw err }
+            if (err) {
+                console.log(err);
+                res.send({ status: 500, message: "Internal Server Error" });
+            }
             else {
                 if (result[0].length > 0) {
                     result[0].forEach(group => {
@@ -82,7 +85,8 @@ app.get('/groups/search/:search', function (req, res) {
             }
         })
     } catch (err) {
-
+        console.log(err);
+        res.send({ status: 500, message: "Internal Server Error" });
     }
 })
 
@@ -99,14 +103,20 @@ app.post('/groups', function (req, res) {
     try {
         let query = "CALL add_new_group(\"" + group.name + "\", \"" + group.description + "\", \"" + group.goal + "\", " + group.admin + ")";
         connection.query(query, function (err, result) {
-            if (err) { throw err }
+            if (err) {
+                console.log(err);
+                res.send({ status: 500, message: 'Internal Server Error' });
+            }
             else {
                 let new_group_id = result[0][0].inserted_id;
                 if (milestones.length != 0) {
                     milestones.forEach(ms => {
                         let query = "CALL Add_group_milestone(" + new_group_id + ", \"" + ms.name + "\", " + ms.order + ")";
                         connection.query(query, function (err, result) {
-                            if (err) { throw err }
+                            if (err) {
+                                console.log(err);
+                                res.send({ status: 500, message: 'Internal Server Error' });
+                            }
                         })
                     });
                 }
@@ -114,6 +124,30 @@ app.post('/groups', function (req, res) {
             }
         })
         console.log("New Group Alert! - " + group.name);
+    }
+    catch (err) {
+        res.send({ status: 500, message: 'Internal Server Error' });
+    }
+})
+
+/**Delete group from database
+ * 
+ * params: group id
+ * return if deleteionwas complete or if there was an error
+ */
+app.delete('/group/:id', function (req, res) {
+    let group = req.params.id;
+    try {
+        let query = "CALL Delete_group(" + group + ")";
+        connection.query(query, function (err, result) {
+            if (err) {
+                console.log(err);
+                res.send({ status: 500, message: 'Internal Server Error' });
+            }
+            else {
+                res.send({ status: 200, message: "Group Deleted" })
+            }
+        })
     }
     catch (err) {
         res.send({ status: 500, message: 'Internal Server Error' });
@@ -130,14 +164,48 @@ app.get('/groups/:id', function (req, res) {
     try {
         let query = "CALL Get_group_information(" + group + ");";
         connection.query(query, function (err, result) {
-            if (err) { throw err }
+            if (err) {
+                console.log(err);
+                res.send({ status: 500, message: "Internal Server Error" });
+            }
             else {
                 let group_data = new Group(result[0][0].id, result[0][0].group_name, result[0][0].description, result[0][0].goal, result[0][0].admin_id)
                 res.send({ group: group_data });
             }
         })
     } catch (err) {
+        console.log(err);
+        res.send({ status: 500, message: "Internal Server Error" });
+    }
+})
 
+/**
+ * Get the members associate with group
+ * 
+ * params: group_id
+ * returns list of group members
+ */
+app.get('/groups/:id/members', function (req, res) {
+    let group = req.params.id;
+    let members = [];
+    try {
+        let query = "CALL Get_group_members(" + group + ")";
+        connection.query(query, function (err, result) {
+            if (err) {
+                console.log(err);
+                res.send({ status: 500, message: "Internal Server Error" });
+            }
+            else {
+                result[0].forEach(mem => {
+                    let member = {userId:mem.user_id, name: mem.full_name, username:mem.username}
+                    members.push(member);
+                })
+                res.send({ status: 200, members: members });
+            }
+        })
+    } catch (err) {
+        console.log(err);
+        res.send({ status: 500, message: "Internal Server Error" });
     }
 })
 
@@ -145,6 +213,29 @@ app.get('/groups/:id', function (req, res) {
  * params: group_id
  * return; list of milestones for the group (milestone_id, name, order)
  */
+app.get('/groups/:id/milestones', function (req, res) {
+    let group = req.params.id;
+    let milestones = [];
+    try {
+        let query = "CALL Get_group_milestones(" + group + ")";
+        connection.query(query, function (err, result) {
+            if (err) {
+                console.log(err);
+                res.send({ status: 500, message: "Internal Server Error" });
+            }
+            else {
+                result[0].forEach(ms => {
+                    let milestone = {milestoneId:ms.milestnoe_id, name: ms.milestone_name, order:ms.order};
+                    milestones.push(milestone);
+                })
+                res.send({ status: 200, milestones: milestones });
+            }
+        })
+    } catch (err) {
+        console.log(err);
+        res.send({ status: 500, message: "Internal Server Error" });
+    }
+})
 
 /**Add a member to a group
  * params: user_id, group_id
@@ -156,35 +247,43 @@ app.post('/groups/members', function (req, res) {
     try {
         let query = "CALL Add_user_to_group(" + user + ", " + group + ");";
         connection.query(query, function (err, result) {
-            if (err) { throw err }
+            if (err) {
+                console.log(err);
+                res.send({ status: 500, message: "Internal Server Error" });
+            }
             else {
                 console.log("user: " + user + " has joined group: " + group);
                 res.send({ status: 200, message: "Welcome to the Group!" });
             }
         })
     } catch (err) {
-
+        console.log(err);
+        res.send({ status: 500, message: "Internal Server Error" });
     }
 })
 
 /**Add post to group
  * params: groupId, memberId, post, timestamp
 */
-app.post('/groups/:id/posts', function(req, res){
+app.post('/groups/:id/posts', function (req, res) {
     let group = req.params.id;
     let user = req.body.user;
     let post = req.body.post;
     try {
-        let query = "CALL Add_new_post("+group+", "+user+", \""+post+"\");";
-        connection.query(query, function(err, result){
-            if(err){throw err}
-            else{
-                console.log("New post in Group - "+ group);
-                res.send({status: 200, message: "Post added to group!"})
+        let query = "CALL Add_new_post(" + group + ", " + user + ", \"" + post + "\");";
+        connection.query(query, function (err, result) {
+            if (err) {
+                console.log(err);
+                res.send({ status: 500, message: "Internal Server Error" });
+            }
+            else {
+                console.log("New post in Group - " + group);
+                res.send({ status: 200, message: "Post added to group!" })
             }
         })
     } catch (err) {
-        
+        console.log(err);
+        res.send({ status: 500, message: "Internal Server Error" });
     }
 })
 
@@ -192,57 +291,69 @@ app.post('/groups/:id/posts', function(req, res){
  * params: post_id, user_id
  * return count of likes, boolean if user liked post
  */
-app.get('/posts/:id/likes/:user', function(req, res){
+app.get('/posts/:id/likes/:user', function (req, res) {
     let post = req.params.id;
     let user = req.params.user;
     try {
-        let query = "CALL Count_post_likes("+post+", "+user+");";
-        connection.query(query, function(err, result){
-            if(err){throw err}
-            else{
-                res.send({status: 200, likes:result[0][0].like_count, memberLiked: result[0][0].member_liked, likeId: result[0][0].like_id});
+        let query = "CALL Count_post_likes(" + post + ", " + user + ");";
+        connection.query(query, function (err, result) {
+            if (err) {
+                console.log(err);
+                res.send({ status: 500, message: "Internal Server Error" });
+            }
+            else {
+                res.send({ status: 200, likes: result[0][0].like_count, memberLiked: result[0][0].member_liked, likeId: result[0][0].like_id });
             }
         })
     } catch (err) {
-        
+        console.log(err);
+        res.send({ status: 500, message: "Internal Server Error" });
     }
 })
 
 /**Add like to post
  * params: post_id, group_id, user_id
  */
-app.post('/posts/:id/likes', function(req, res){
+app.post('/posts/:id/likes', function (req, res) {
     let post = req.params.id;
     let group = req.body.group;
     let user = req.body.user;
     try {
-        let query = "CALL add_like_to_post("+post+", "+group+", "+user+");";
-        connection.query(query, function(err, result){
-            if(err){throw err}
-            else{
-                res.send({status:200, likeId: result[0][0].like_id})
+        let query = "CALL add_like_to_post(" + post + ", " + group + ", " + user + ");";
+        connection.query(query, function (err, result) {
+            if (err) {
+                console.log(err);
+                res.send({ status: 500, message: "Internal Server Error" });
+            }
+            else {
+                res.send({ status: 200, likeId: result[0][0].like_id })
             }
         })
     } catch (err) {
-        
+        console.log(err);
+        res.send({ status: 500, message: "Internal Server Error" });
     }
 })
 
 /**Delete like from post
  * params: post_id, group_id, user_id
  */
-app.delete('/likes/:id', function(req, res){
+app.delete('/likes/:id', function (req, res) {
     let like = req.params.id;
     try {
-        let query = "DELETE FROM anchr.likes WHERE anchr.likes.id = "+like;
-        connection.query(query, function(err, result){
-            if(err){throw err}
-            else{
-                res.send({status:200})
+        let query = "DELETE FROM anchr.likes WHERE anchr.likes.id = " + like;
+        connection.query(query, function (err, result) {
+            if (err) {
+                console.log(err);
+                res.send({ status: 500, message: "Internal Server Error" });
+            }
+            else {
+                res.send({ status: 200 })
             }
         })
     } catch (err) {
-        
+        console.log(err);
+        res.send({ status: 500, message: "Internal Server Error" });
     }
 })
 
@@ -250,18 +361,22 @@ app.delete('/likes/:id', function(req, res){
  * params: post_id
  * return count of comments
  */
-app.get('/posts/:id/comments/count', function(req, res){
+app.get('/posts/:id/comments/count', function (req, res) {
     let post = req.params.id;
     try {
-        let query = "CALL Count_post_comments("+post+");";
-        connection.query(query, function(err, result){
-            if(err){throw err}
-            else{
-                res.send({status: 200, count:result[0][0].count});
+        let query = "CALL Count_post_comments(" + post + ");";
+        connection.query(query, function (err, result) {
+            if (err) {
+                console.log(err);
+                res.send({ status: 500, message: "Internal Server Error" });
+            }
+            else {
+                res.send({ status: 200, count: result[0][0].count });
             }
         })
     } catch (err) {
-        
+        console.log(err);
+        res.send({ status: 500, message: "Internal Server Error" });
     }
 })
 
@@ -270,51 +385,59 @@ app.get('/posts/:id/comments/count', function(req, res){
  * params: group_id
  * return list of posts (no comments) to display on the group page ( post_id, member_id, member_name, post, timestamp)
  */
-app.get('/groups/:id/posts', function(req, res){
+app.get('/groups/:id/posts', function (req, res) {
     let group = req.params.id;
     let posts = [];
     try {
-        let query = "CALL Get_top10_posts("+group+");";
-        connection.query(query, function(err, result){
-            if(err){throw err}
-            else{
+        let query = "CALL Get_top10_posts(" + group + ");";
+        connection.query(query, function (err, result) {
+            if (err) {
+                console.log(err);
+                res.send({ status: 500, message: "Internal Server Error" });
+            }
+            else {
                 result[0].forEach(post => {
                     let found_post = new Post(post.id, post.group_id, post.group_name, post.full_name, post.username, post.post, post.born_date);
                     posts.push(found_post);
                 });
             }
 
-            res.send({status: 200, posts:posts});
+            res.send({ status: 200, posts: posts });
         })
     } catch (err) {
-        
+        console.log(err);
+        res.send({ status: 500, message: "Internal Server Error" });
     }
 })
 
- /**Get next 10 most recent posts associated with group
- *
- * params: group_id, last_post_id
- * return list of posts (no comments) to display on the group page ( post_id, member_id, member_name, post, timestamp)
- */
-app.get('/groups/:id/posts/:lastPost', function(req, res){
+/**Get next 10 most recent posts associated with group
+*
+* params: group_id, last_post_id
+* return list of posts (no comments) to display on the group page ( post_id, member_id, member_name, post, timestamp)
+*/
+app.get('/groups/:id/posts/:lastPost', function (req, res) {
     let group = req.params.id;
     let last_post_id = req.params.lastPost;
     let posts = [];
     try {
-        let query = "CALL Get_next10_posts("+group+", "+last_post_id+");";
-        connection.query(query, function(err, result){
-            if(err){throw err}
-            else{
+        let query = "CALL Get_next10_posts(" + group + ", " + last_post_id + ");";
+        connection.query(query, function (err, result) {
+            if (err) {
+                console.log(err);
+                res.send({ status: 500, message: "Internal Server Error" });
+            }
+            else {
                 result[0].forEach(post => {
                     let found_post = new Post(post.id, post.group_id, post.group_name, post.full_name, post.username, post.post, post.born_date);
                     posts.push(found_post);
                 });
             }
 
-            res.send({status: 200, posts:posts});
+            res.send({ status: 200, posts: posts });
         })
     } catch (err) {
-        
+        console.log(err);
+        res.send({ status: 500, message: "Internal Server Error" });
     }
 })
 
@@ -322,21 +445,25 @@ app.get('/groups/:id/posts/:lastPost', function(req, res){
  *
  * params: post_id, member_id, comment
  */
-app.post('/posts/:id/comments', function(req, res){
+app.post('/posts/:id/comments', function (req, res) {
 
     let post = req.params.id;
     let user = req.body.user;
     let comment = req.body.comment;
     try {
-        let query = "CALL Add_comment_to_post("+post+", "+user+", \""+comment+"\");";
-        connection.query(query, function(err, result){
-            if(err){throw err}
-            else{
-                res.send({status:200, message:"Comment added to post"});
+        let query = "CALL Add_comment_to_post(" + post + ", " + user + ", \"" + comment + "\");";
+        connection.query(query, function (err, result) {
+            if (err) {
+                console.log(err);
+                res.send({ status: 500, message: "Internal Server Error" });
+            }
+            else {
+                res.send({ status: 200, message: "Comment added to post" });
             }
         })
     } catch (err) {
-        
+        console.log(err);
+        res.send({ status: 500, message: "Internal Server Error" });
     }
 
 })
@@ -351,23 +478,27 @@ app.post('/posts/:id/comments', function(req, res){
  * params: post_id
  * return: list of comments (member_id, comment, timestamp)
  */
-app.get('/posts/:id/comments', function(req, res){
+app.get('/posts/:id/comments', function (req, res) {
     let post = req.params.id;
     let comments = [];
     try {
-        let query = "CALL Get_post_comments("+post+");";
-        connection.query(query, function(err, result){
-            if(err){throw err}
-            else{
+        let query = "CALL Get_post_comments(" + post + ");";
+        connection.query(query, function (err, result) {
+            if (err) {
+                console.log(err);
+                res.send({ status: 500, message: "Internal Server Error" });
+            }
+            else {
                 result[0].forEach(comment => {
                     let found_comment = new Comment(comment.id, comment.post_id, comment.full_name, comment.username, comment.comment, comment.date);
                     comments.push(found_comment);
                 })
-                res.send({status:200, comments:comments});
+                res.send({ status: 200, comments: comments });
             }
         })
     } catch (err) {
-        
+        console.log(err);
+        res.send({ status: 500, message: "Internal Server Error" });
     }
 })
 
@@ -401,7 +532,7 @@ class Group {
  */
 class Post {
 
-    constructor(id, group_id, group_name, user_name, username, post, date){
+    constructor(id, group_id, group_name, user_name, username, post, date) {
         this.id = id;
         this.groupId = group_id;
         this.groupName = group_name;
@@ -415,8 +546,8 @@ class Post {
 /**holds comment information 
  * 
 */
-class Comment{
-    constructor(id, post_id, user, username, comment, date){
+class Comment {
+    constructor(id, post_id, user, username, comment, date) {
         this.id = id;
         this.postId = post_id;
         this.user = user;
